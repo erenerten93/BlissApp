27a69f1baa06a140b2a3cce861491a3e7210721f
EREN <erteneren93@gmail.com>
2023-02-17 14:46:02 +0000

    Initial Commit

---
+++ BlissApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlissApp
+//
+//  Created by Eren Erten on 17.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BlissAppUITests/BlissAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BlissAppUITestsLaunchTests.swift
+//  BlissAppUITests
+//
+//  Created by Eren Erten on 17.02.2023.
+//
+
+import XCTest
+
+final class BlissAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
